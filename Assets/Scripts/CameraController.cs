8eb162902f4844fd607acdfea203535aebb03ce2
Ignacio Villalobos Salas <nachovs91@outlook.es>
2015-12-18 23:32:50 +0100

    Added player physics, control from keyboard and camera tracking

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject target;
+
+    private Vector3 offset;
+
+	void Start () {
+        offset = transform.position - target.transform.position;
+	}
+	
+	// LateUpdate is called after update
+	void LateUpdate () {
+        transform.position = target.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Vector3 movement;
+    public float Speed = 1;
+
+
+    void Start() {
+        //Component Rigidbody from the object 
+        rb = GetComponent<Rigidbody>();
+    }
+    //FixedUpdate is called before performing any physics calculations
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * Speed);
+    }
+}
