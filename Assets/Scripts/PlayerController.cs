730022cfdfd481e95bcc254c61e15833d7f13d58
Ignacio Villalobos Salas <nachovs91@outlook.es>
2015-12-26 19:31:52 +0100

    Win and lose conditions and labels , improve the lvl 0

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,16 +8,30 @@ public class PlayerController : MonoBehaviour {
     private Vector3 movement;
     public float Speed = 1;
     private int CollectableItemsCount;
-
+    public int WinScore;
     public Text scoretext;
+    public Text ResultText;
 
 
     void Start() {
         //Component Rigidbody from the object 
         rb = GetComponent<Rigidbody>();
         CollectableItemsCount=0;
-
         SetScoreText();
+    }
+    void Update() {
+
+        if (WinScore.Equals(CollectableItemsCount)) {
+            //You win !!!!
+            ResultText.text = "YOU WIN!!!!!!";
+            
+        }
+        if (rb.transform.position.y < -10) {
+            // You fall!! and lose :(
+            ResultText.text = "YOU LOSE :(";
+            
+        }
+
     }
     //FixedUpdate is called before performing any physics calculations
     void FixedUpdate() {
@@ -28,8 +42,6 @@ public class PlayerController : MonoBehaviour {
         movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         rb.AddForce(movement, ForceMode.Acceleration);
     #endif
-
-
 
         /*From accelerometer*/
         rb.AddForce(new Vector3(Input.acceleration.x,Input.acceleration.z,Input.acceleration.y), ForceMode.Acceleration);
